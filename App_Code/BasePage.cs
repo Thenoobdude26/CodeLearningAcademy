7d1e7685066a2223c1396f2514358a65be2260bb
thenoobdude26 <tatendacoolboy@gmail.com>
2026-03-04 10:45:38 +0800

    dasd

--- App_Code/BasePage.cs
+++ App_Code/BasePage.cs
@@ -1,9 +1,37 @@
 ﻿using System;
 using System.Web.UI;
 
-namespace CodeLearningAcademy.App_Code
+//namespace CodeLearningAcademy.App_Code
+//{
+//    public class BasePage
+//    {
+//    }
+//}
+
+
+public class  BasedPage : Page
 {
-    public class BasePage
+    protected bool IsLoggedIn => AuthHelper.IsLoggedIn;
+    protected int CurrentUserId => AuthHelper.UserID;
+    protected string CurrentUserName => AuthHelper.Username;
+    protected string CurrentRole => AuthHelper.Role;
+    protected bool IsAdmin => AuthHelper.IsAdmin;
+    protected bool IsLecturer => AuthHelper.IsLecturer;
+    protected bool IsStudent => AuthHelper.IsStudent;
+
+    //redircects login if not loggged in
+
+    protected void RequireLogin()
     {
+        if (!IsLoggedIn)
+            Response.Redirect("~/Login.aspx", true);
+    }
+
+    //redirects to home if not admin
+    protected void RequireAdmin()
+    {
+        RequireLogin();
+        if (!IsAdmin)
+            Response.Redirect("~/Default.aspx", true);
     }
 }
